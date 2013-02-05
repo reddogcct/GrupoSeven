e20635d2927952e3c62b3608c0d5177de605ee4e
Rhuansantos <rhuansantos7@gmail.com>
2013-02-05 11:00:40 -0200

    Fazendo App do facebook

---
+++ CG_Extreme/Facebook/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+	<head>
+	<title>CG Extreme</title>
+	<link href='style/main.css' rel='stylesheet' type='text/css'>
+	<meta name="keywords" content="cgextreme, Seven, Fullsail, Citibank hall, Chance Glasco, Nathaniel Howe, Sebastian Krys, Cordy Rierson, Laurie Brugger, Jayson Whitmore">
+	<meta name="description" content="cgextreme web site">
+	<meta name="author" content="seven media">
+	<meta charset="utf-8">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	</head>
+	
+<body>
+
+	<div class="mainDiv">
+		<div id="menu">
+			<ul>
+				<li><img src="image/home/icone-home.png" alt="" title""></li>
+				<li><img src="image/home/icone-home.png" alt="" title""></li>
+				<li><img src="image/home/icone-home.png" alt="" title""></li>
+				<li><img src="image/home/icone-home.png" alt="" title""></li>
+			</ul>
+		</div><!-- close menu-->
+
+		<div id="contentHome">
+		
+		</div><!-- close content-->
+	</div><!--close mainDiv-->
+
+
+</body>
+</html>
---
+++ CG_Extreme/Facebook/js/main.js
@@ -0,0 +1,6 @@
+// Java Script
+$(document).ready(function(e) {
+	$("#menu").animate({
+		marginTop:"0px",
+	},3000);
+});
